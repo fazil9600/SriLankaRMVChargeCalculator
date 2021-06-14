e0a8b726c567d513942583466ec162ea70034eea
fazil9600 <fazil9600@gmail.com>
2021-06-14 21:55:38 +0530

    second commit

---
+++ LoginCover.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace SriLankaRMVCalculator
+{
+    partial class LoginCover
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginCover));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pb_initial_load = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(559, 102);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(112, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SRI LANKA RMV CHARGE CALCULATOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tekton Pro Ext", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(199, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please hang in there.....";
+            // 
+            // pb_initial_load
+            // 
+            this.pb_initial_load.BackColor = System.Drawing.Color.Black;
+            this.pb_initial_load.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.pb_initial_load.Location = new System.Drawing.Point(12, 279);
+            this.pb_initial_load.Name = "pb_initial_load";
+            this.pb_initial_load.Size = new System.Drawing.Size(569, 10);
+            this.pb_initial_load.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // LoginCover
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(593, 301);
+            this.Controls.Add(this.pb_initial_load);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginCover";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginCover";
+            this.Load += new System.EventHandler(this.LoginCover_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar pb_initial_load;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ LoginCover.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SriLankaRMVCalculator
+{
+    public partial class LoginCover : Form
+    {
+        public LoginCover()
+        {
+            InitializeComponent();
+        }
+        int startpoint = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startpoint += 1;
+            pb_initial_load.Value = startpoint;
+            if (pb_initial_load.Value == 100)
+            {
+                pb_initial_load.Value = 0;
+                timer1.Stop();
+                RMVMain log = new RMVMain();
+                this.Hide();
+                log.Show();
+            }
+
+        }
+
+        private void LoginCover_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
